2bc2fb802c73b9806e94536c6693d1e9bd593b58
mookbin <beeen1@naver.com>
2023-05-13 01:59:01 +0900

    feat : chat페이지 뼈대 구현중

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,5 +1,54 @@
 import React from "react";
-
+import {
+  EmptyBox, ChatDetail, LastCommentDetails, LastComment, SubText, YourNickname, YourInfo, ListA, ListLi, ListUl, MynameDiv, ListbarNav, SidebarNav, ChatList, ChatDiv, Img
+} from "../styles/chatPageSt";
 export const ChatPage = () => {
-  return <div>ChatPage</div>;
+  return (
+    <>
+      <ChatDiv>
+        <div></div>
+        <ChatList>
+          <SidebarNav className="sidebar">
+            <a href=" " style={{ textDecoration: "none" }}>
+              {/* <Img className="profile-image" src="https://dnvefa72aowie.cloudfront.net/origin/profile/profile_default.png" alt="">
+              </Img> */}
+            </a>
+          </SidebarNav>
+          <ListbarNav>
+            <MynameDiv>본인 닉네임</MynameDiv>
+            <ListUl>
+              <ListLi>
+                <ListA href=" ">
+                  <div style={{ marginRight: "8px", height: " 40px" }}>
+                    상대 프로필 사진
+                  </div>
+                  <div style={{ flex: "1 0 0%", width: "0px" }}>
+                    <YourInfo>
+                      <YourNickname>상대 닉네임</YourNickname>
+                    </YourInfo>
+                    <SubText>
+                      <span>지역</span>
+                      <span>.</span>
+                      <span>3달 전</span>
+                    </SubText>
+                    <LastComment>
+                      <LastCommentDetails>마지막 대화 내용</LastCommentDetails>
+                    </LastComment>
+                  </div>
+                </ListA>
+
+              </ListLi>
+
+            </ListUl>
+          </ListbarNav>
+        </ChatList>
+        <ChatDetail>
+          <EmptyBox>
+            <div>채팅할 상대를 선택해주세요.</div>
+          </EmptyBox>
+        </ChatDetail>
+        <div></div>
+      </ChatDiv >
+    </>
+  );
 };
