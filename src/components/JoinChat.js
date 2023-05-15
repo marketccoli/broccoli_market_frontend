dab15ee6a83169533d3ddf3699bf9f1c9e3e98a3
mookbin <beeen1@naver.com>
2023-05-15 21:54:26 +0900

    feat : 전체 채팅 목록 불러오기 구현

--- src/components/JoinChat.js
+++ src/components/JoinChat.js
@@ -6,10 +6,10 @@ const socket = io.connect("http://localhost:3002");
 
 
 function JoinChat() {
-  //username과 room 상태 변수를 생성
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
+    //username과 room 상태 변수를 생성
+    const [username, setUsername] = useState("");
+    const [room, setRoom] = useState("");
+    const [showChat, setShowChat] = useState(false);
 
 
     //joinRoom 함수를 생성하고, 클라이언트에서 입력한 username과 room 정보를 이용해 해당 room에 클라이언트를 추가하는 "join_room" 이벤트를 발생
@@ -24,18 +24,18 @@ function JoinChat() {
     return (
         <div className="App">
             {/*username, room 정보를 입력할 수 있는 input 태그와 joinRoom 함수를 실행하는 버튼을 출력 */}
-            {!showChat ? (
+            {/* {!showChat ? (
                 <div>
                     <h3> 채팅참가</h3>
                     <input type="text" placeholder="이름 입력" onChange={(event) => { setUsername(event.target.value) }}></input>
                     <input type="text" placeholder="Room Id" onChange={(event) => { setRoom(event.target.value) }}></input>
                     <button onClick={joinRoom}>채팅에 참여하기</button>
                 </div>
-            ) : (
-                < Chatroom socket={socket} username={username} room={room} />
-            )}
-    </div>
-  );
+            ) : ( */}
+            < Chatroom socket={socket} username={username} room={room} />
+            {/* )} */}
+        </div>
+    );
 }
 //  Chat 컴포넌트에서는 props로 전달받은 socket, username, room 정보를 이용하여 실시간 채팅을 구현
 // Chat 컴포넌트에 socket, username, room 정보를 props로 전달
